4fc9578bab9cf2060211d298c34115d72bc07ab9
Никита Жданов <n3ktarinka@yandex.ru>
2024-08-01 08:08:18 +0300

    fix project

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('pages/', include('pages.urls', namespace='pages')),
     path('', include('blog.urls', namespace='blog')),
     path('auth/registration/', CreateView.as_view(
-        template_name='registration/registration_form.html', 
+        template_name='registration/registration_form.html',
         form_class=UserCreationForm, success_url=reverse_lazy('blog:index'),),
         name='registration',),
 ]
